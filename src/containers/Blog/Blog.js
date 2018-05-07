296c5eddb1cc00403bcf674604f844eac28fc402
JeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2018-05-06 21:48:15 -0300

    Lesson 152 - Transforming Data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,36 +7,43 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-        posts: [],
-    }
+  state = {
+    posts: [],
+  }
 
-    componentDidMount() {
-        axios.get('https://jsonplaceholder.typicode.com/posts')
-             .then(res => {
-                 this.setState({posts: res.data});
-                //  console.log(res.data);
-             })
-             .catch(err => console.log(err));
-    }
-    render () {
-        const posts = this.state.posts.map(p => {
-            return <Post key={p.id} title={p.title} author={p.author}/>
-        });
-        return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+          .then(res => {
+            const posts = res.data.slice(0, 4);
+            const updatedPosts = posts.map(p => {
+              return({
+                ...p,
+                author: 'Max',
+              });
+            });
+
+            this.setState({posts: updatedPosts});
+          })
+          .catch(err => console.log(err));
+  }
+  render () {
+    const posts = this.state.posts.map(p => {
+      return <Post key={p.id} title={p.title} author={p.author}/>
+    });
+    return (
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
