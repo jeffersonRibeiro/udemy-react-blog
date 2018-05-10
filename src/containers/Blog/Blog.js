b8fb5f489f4b9c334f7334e3061a6e49bae5b30b
JeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2018-05-09 23:23:49 -0300

    Lesson 188 - Styling the Active Route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Link }from 'react-router-dom';
+import { Route, NavLink }from 'react-router-dom';
 
 
 import './Blog.css';
@@ -15,12 +15,12 @@ class Blog extends Component {
         <header>
           <nav>
             <ul>
-              <li><Link to="/">Home</Link></li>
-              <li><Link to={{
+              <li><NavLink exact to="/">Home</NavLink></li>
+              <li><NavLink to={{
                 pathname: '/new-post',
                 hash: '#submit',
                 search: '?quick=true',
-              }}>New Posts</Link></li>
+              }}>New Posts</NavLink></li>
             </ul>
           </nav>
         </header>
