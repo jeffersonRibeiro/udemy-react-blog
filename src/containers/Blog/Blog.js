9cb0368278adcc3217f3758d444042e17d65b214
JeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2018-05-09 22:10:39 -0300

    Lesson 177 - Setting Up the router Package

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
@@ -56,12 +53,12 @@ class Blog extends Component {
         <section className="Posts">
           {posts}
         </section>
-        <section>
+        {/* <section>
           <FullPost id={this.state.selectedPostId} />
         </section>
         <section>
           <NewPost />
-        </section>
+        </section> */}
       </div>
     );
   }
