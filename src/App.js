9e23ab0ffaa53552ed6477e46ad04d0582e979c0
JeffersonRibeiro <jefferson.ribeiro.contato@gmail.com>
2018-05-12 01:29:28 -0300

    Basename configuration for browserRouter

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      /* http://example.com/my-app */
+      // <BrowserRouter basename="/my-app">
       <BrowserRouter>
         <div className="App">
           <Blog />
